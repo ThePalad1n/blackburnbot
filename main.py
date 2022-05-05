d95f33994fd600d4e56cf8fdcdebea0d1b25b637
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2022-05-05 13:41:05 +0000

    working on localized econ

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ intents = discord.Intents().all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='re ', intents=intents)
 
-@bot.command(name='rarity', help='List of quotes and how rare')
-async def rarity(ctx):
-    if ctx.message.content.startswith('re rarity'):
-      await ctx.message.channel.send("Total: 90\n Common: 29\n Uncommon: 19\n Rare: 15\n Epic: 11\n Legendary: 8\n God: 3\n Hidden: ???\n")
+@bot.command(name='rerarity', help='List of quotes and how rare')
+async def rerarity(ctx):
+    if ctx.message.content.startswith('re rerarity'):
+      await ctx.message.channel.send("Total: 91\n Common: 29\n Uncommon: 20\n Rare: 15\n Epic: 11\n Legendary: 8\n God: 3\n Hidden: ???\n")
 
 
 @bot.command(name='b', help='The quotes youve always wanted')
 async def b(ctx):
     if ctx.message.content.startswith('re b'):
 
-      dict = {"Im gonna blow up the plane. " : 3,"I quit. " : 1,"Im gonna shoot up work. " : 2,"FYI I got dibs on bed. " : 1,"Shoulda got her while she was vulnerable." : 3,"I loved Gina." : 4,"I love you " : 2,"Connor ima steal my girlfriend back " : 6,"Thats ass my guy. " : 1,"Ill even call you daddy." : 2,"I mean I will definitely watch a lot of porn tonight but crying in the shower is a gametime decision " : 6,"if you wanted to stop the stupidity you shoulda told my dad to pull out " : 4,"Lets go steal MILFs kayaks." : 2,"I dont need a 401k this is my retirement plan *gunshot* " : 4,"she can, i make sure to send it through the phone " : 1,"kelly wants me " : 3,"youre gonna roast me while im having a shit day then expect a roasting back " : 4,"Anyone tryn to play some catan? " : 1,"I am so tired. " : 1,"Evan you can have all the beer at my house " : 1,"Who the fuck ate my chicken nuggets " : 3,"Im gonna fuck a donkey " : 1,"nikhil i got you\n i offer payment plans for maintenance\n pay me in taco bell\n party packs " : 4,"idk why you expect me to behave normally " : 3,"go home pussy\n boys peep the text chat\n barps is LITERALLY ASKING FOR IT\n MIGHT HAVE KILLED HIM " : 2,"Im talking myself into getting angry." : 4, "I need to get alcohol poisioning " : 2, "he cornered me in highschool when the group was chillin in valley sqaure\n tried to suck me off for half hour " : 4,"no shot " : 1,"nah not a fan of gays\n unless theyre lesbians\n huge fan of those " : 3,"I read energy real well " : 1,"cuz they know i wanna fuck " : 1,"i love harry potter! " : 1,"smith ill put my pp in your mouth while youre sleeping " : 4,"are we allowed to talk to maya on this trip or is she off limits " : 5,"my gay side needs to know if im attractive looking to guys " : 4,"id say i love you katrina but weve been down that road " : 5,"i love you connor ": 1,"everyones awesome\n unless you point a gun at me " : 2,"definitely dont wanna work either\n yeah but now im dying " : 1,"i come here and get pissed off before 11am everyday now " : 1,"ill put your liver through the guantlet." : 2,"Im angry." : 5,"look at who you wanna be in the future and ask yourself if that guy jerks off on the daily " : 6,"oh bet so yeah all i was thinking about was mia khalifa and it was a STRUGGLE to get to bed " : 3,"Nick definitely should have gotten a DUI on the boat " : 2,"IM GONNA BE RAILING HOT CHOCO ALL DAY " : 2,"im 1 and half blasts in rn and its hard to contain myself in an office " : 4,"that seems very specific cuz i have no clue what sql is but you wouldnt catch me doing it " : 1,"Yeah he's getting his pp slapped again " : 2,"KEVIN DOESNT WANNA SEE ME LOSE MY SHIT " : 3,"I AM A HUGE PERV, YOU KNOW THIS " : 1,"matt take drugs " : 2,"i just wanna see her naked " : 1,"jess's sexy voice is right next to me " : 1,"im getting so high tonight that i wont be able to have a conversation " : 5, "Kevin raped my mom.... metaphorically. " : 1,"Tell kevin to go fuck himself " : 3,"Kevins wackin my dick rn. " : 5,"Its not a privilege to drive." : 2,"shes a stick with no assets " : 3,"coulda been a way better summer cuz if she was giving me daily head she wouldnt have thought i was fucking her mom\n big brain " : 3,"i fucked a milf in one pump\n i am proud " : 3,"im a freak\n you all should know this " : 1,"what have i done to her\n musta been the cheating " : 1,"ryann ratcliffe is apparently drinking more than me and woody combined\n and now shes fat " : 2,"shes definitely a screamer " : 1,"Not that Paris " : 3,"Im a jumper " : 1,"so she knows how to ride the right way " : 1,"I'll put it together retard " : 3,"i just found another sexy female\n not sure where she came from " : 2,"Im *investing* " : 5,"ill suck you off woody " : 5,"i need sex " : 2,"boys i wanna commit robbery " : 1,"I AM STIMULATED " : 1, "I do what I want, when I want. " : 3, "You can have the couch, $200 a month " : 4, "The things I would do to her. " : 1, "Im not trying to piss you off I just have big goals " : 2, "Tested positive this morning, yeah its lowkey amazing " : 2, "I ain't no side hoe " : 2, "80% chance I wonnt be up that late" : 1, "Yo your mom is still single rn?! I'll be your stepdad." : 5, "Special Airdrop!\n" : 7}		
+      dict = {"Im gonna blow up the plane. " : 3,"I quit. " : 1,"Im gonna shoot up work. " : 2,"FYI I got dibs on bed. " : 1,"Shoulda got her while she was vulnerable." : 3,"I loved Gina." : 4,"I love you " : 2,"Connor ima steal my girlfriend back " : 6,"Thats ass my guy. " : 1,"Ill even call you daddy." : 2,"I mean I will definitely watch a lot of porn tonight but crying in the shower is a gametime decision " : 6,"if you wanted to stop the stupidity you shoulda told my dad to pull out " : 4,"Lets go steal MILFs kayaks." : 2,"I dont need a 401k this is my retirement plan *gunshot* " : 4,"she can, i make sure to send it through the phone " : 1,"kelly wants me " : 3,"youre gonna roast me while im having a shit day then expect a roasting back " : 4,"Anyone tryn to play some catan? " : 1,"I am so tired. " : 1,"Evan you can have all the beer at my house " : 1,"Who the fuck ate my chicken nuggets " : 3,"Im gonna fuck a donkey " : 1,"nikhil i got you\n i offer payment plans for maintenance\n pay me in taco bell\n party packs " : 4,"idk why you expect me to behave normally " : 3,"go home pussy\n boys peep the text chat\n barps is LITERALLY ASKING FOR IT\n MIGHT HAVE KILLED HIM " : 2,"Im talking myself into getting angry." : 4, "I need to get alcohol poisioning " : 2, "he cornered me in highschool when the group was chillin in valley sqaure\n tried to suck me off for half hour " : 4,"no shot " : 1,"nah not a fan of gays\n unless theyre lesbians\n huge fan of those " : 3,"I read energy real well " : 1,"cuz they know i wanna fuck " : 1,"i love harry potter! " : 1,"smith ill put my pp in your mouth while youre sleeping " : 4,"are we allowed to talk to maya on this trip or is she off limits " : 5,"my gay side needs to know if im attractive looking to guys " : 4,"id say i love you katrina but weve been down that road " : 5,"i love you connor ": 1,"everyones awesome\n unless you point a gun at me " : 2,"definitely dont wanna work either\n yeah but now im dying " : 1,"i come here and get pissed off before 11am everyday now " : 1,"ill put your liver through the guantlet." : 2,"Im angry." : 5,"look at who you wanna be in the future and ask yourself if that guy jerks off on the daily " : 6,"oh bet so yeah all i was thinking about was mia khalifa and it was a STRUGGLE to get to bed " : 3,"Nick definitely should have gotten a DUI on the boat " : 2,"IM GONNA BE RAILING HOT CHOCO ALL DAY " : 2,"im 1 and half blasts in rn and its hard to contain myself in an office " : 4,"that seems very specific cuz i have no clue what sql is but you wouldnt catch me doing it " : 1,"Yeah he's getting his pp slapped again " : 2,"KEVIN DOESNT WANNA SEE ME LOSE MY SHIT " : 3,"I AM A HUGE PERV, YOU KNOW THIS " : 1,"matt take drugs " : 2,"i just wanna see her naked " : 1,"jess's sexy voice is right next to me " : 1,"im getting so high tonight that i wont be able to have a conversation " : 5, "Kevin raped my mom.... metaphorically. " : 1,"Tell kevin to go fuck himself " : 3,"Kevins wackin my dick rn. " : 5,"Its not a privilege to drive." : 2,"shes a stick with no assets " : 3,"coulda been a way better summer cuz if she was giving me daily head she wouldnt have thought i was fucking her mom\n big brain " : 3,"i fucked a milf in one pump\n i am proud " : 3,"im a freak\n you all should know this " : 1,"what have i done to her\n musta been the cheating " : 1,"ryann ratcliffe is apparently drinking more than me and woody combined\n and now shes fat " : 2,"shes definitely a screamer " : 1,"Not that Paris " : 3,"Im a jumper " : 1,"so she knows how to ride the right way " : 1,"I'll put it together retard " : 3,"i just found another sexy female\n not sure where she came from " : 2,"Im *investing* " : 5,"ill suck you off woody " : 5,"i need sex " : 2,"boys i wanna commit robbery " : 1,"I AM STIMULATED " : 1, "I do what I want, when I want. " : 3, "Evan, you can have the couch, $200 a month " : 4, "The things I would do to her. " : 1, "Im not trying to piss you off I just have big goals " : 2, "Tested positive this morning, yeah its lowkey amazing " : 2, "I ain't no side hoe " : 2, "80% chance I wont be up that late" : 1, "Yo your mom is still single rn?! I'll be your stepdad." : 5, "Special Airdrop!\n" : 7, "Who is kyle can?" : 2}		
 
     item = random.choice(list(dict.items()))
     x,y = item
@@ -62,6 +62,53 @@ async def b(ctx):
         return 0;
 
 
+@bot.command(name='cjtrarity', help='List of quotes and how rare')
+async def cjtrarity(ctx):
+    if ctx.message.content.startswith('re cjtrarity'):
+      await ctx.message.channel.send("Total:30 \n Common: 8\n Uncommon: 6\n Rare: 6\n Epic: 5\n Legendary: 5\n God: 2\n Hidden: ???\n")
+
+@bot.command(name='cjt', help='The quotes youve always wanted')
+async def cjt(ctx):
+    if ctx.message.content.startswith('re cjt'):
+
+      dict = {"Whats goin on guys, CJT here." : 5, "Kobra Kai" : 4, "Cum" : 1, "Cum Burger" : 2, "Columbus Kai" : 4, "Burger\n Bus\n" : 6, "*COUGH" : 2, "*WHEEZE" : 3, "Krazy Kai" : 1, "Kris Kringle" : 4, "OH\n My Christmas" : 6, "Antidrunk\n Active\n YEEEOWAH" : 3,"King Kai" : 1, "Cheeseburger Eddie" : 4, "Cheeseburger Walrus" : 3, "Lemme suc ur dic" : 3, "Let me see it" : 1, "Imma twist ur dick" : 1, "YEEEEEEOOOWAH" : 6, "The Biiiiiig\n Bus" : 5, "Wanna makeout?":3, "Omg\n Im gonna see a woman\n One I can penetrate" : 5, "*hand on gents" : 2, "Billy." : 2, "Wheres blobo": 7, "I\n Love Cats" : 1, "Kringle Kris" : 5, "Let me fuck you" : 1, "11/10" : 2, "Im 13/10\n and im never 13 out of 10" : 3, "Kaioken" : 4, "value" : 5, "Mmm Hmmm" : 3, "im gonna cuck ur soc" : 3, "big soc" : 2, "big suc" : 1, "meat soc" : 4}		
+
+    item = random.choice(list(dict.items()))
+    x,y = item
+    
+    if (y == 7):
+        await ctx.message.channel.send(x + " (Secret Rare)\n")
+        myid = '<@189552961389592578>'
+        await ctx.message.channel.send(myid)
+    elif(y == 1):
+        await ctx.message.channel.send(x + " (Common)")
+    elif(y == 2): 
+        await ctx.message.channel.send(x + " (Uncommon)")
+    elif(y == 3):
+        await ctx.message.channel.send(x + " (Rare)")
+    elif(y == 4):
+        await ctx.message.channel.send(x + " (Epic)")
+    elif(y == 5):   
+        await ctx.message.channel.send(x + " (Legendary)")
+    elif(y == 6):
+        await ctx.message.channel.send(x + " (Godly)")
+    else:
+        return 0;
+
+
+
+
+@bot.command(name='cjthere', help='Word Masher')
+async def cjthere(ctx):
+    if ctx.message.content.startswith('re cjthere'):
+
+      xxx = ["Cobra", "Cum", "Cheeseburger", "King", "Eldridge", "Burger"]
+      xx = random.choice(xxx)
+      yyy = ["Kai", "Eddie", "Walrus", "Suc", "Bus"]
+      yy = random.choice(yyy)
+
+      await ctx.message.channel.send(xx + " " + yy)
+      
 @bot.command()
 async def pushP(ctx):
     print('We have logged in as {0.user}'.format(client))
@@ -211,7 +258,7 @@ async def news(ctx):
 
 
 #beginning of the weather command
-
+'''
 api_key = os.getenv('WEATHER_TOKEN')
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
@@ -262,7 +309,7 @@ async def weather(ctx, *, city: str):
     else:
         await channel.send("City not found.")
 
-
+'''
 
 @bot.command(name='gm', help='Good Morning')
 async def gm(ctx):
@@ -273,6 +320,17 @@ async def gm(ctx):
 async def re(ctx):
     if ctx.message.content.startswith('re re'):
         await ctx.message.channel.send("re")
+
+@bot.command(name='shutup', help='bot on bot action')
+async def shutup(ctx):
+    if ctx.message.content.startswith('re shutup'):
+        await ctx.message.channel.send(";compile\n\'\'\'c\n #include <stdio.h>\n int main() {\n printf(\'Aye yo shut up!\');\n }\n\'\'\')")
+
+
+                                       
+                                       
+
+
 
 
       
